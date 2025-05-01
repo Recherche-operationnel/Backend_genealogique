3e6d79d9e4c161d9e16825dd883f8e9ffea49919
cgnomc1 <cgnomc@gmail.com>
2025-05-01 16:19:08 +0100

    first commit

--- arbre/graph_algorithms.py
+++ arbre/graph_algorithms.py
@@ -44,7 +44,56 @@ def bellman_ford(start_id, goal_id):
 def prim(start_id=None):
     """
     Algorithme de Prim pour arbre couvrant minimum (MST)
+    
+    Entrée:
+        graph: un dictionnaire où les clés sont les IDs des personnes et 
+               les valeurs sont des listes de tuples (id_dest, poids)
+    
+    Sortie:
+        Un tuple (arbre_sommets, arbre_aretes, poids_total) représentant l'arbre couvrant de poids minimum
+    
     """
+    graph = build_graph()
+    relations = FamilyRelation.objects.all()
+    print(graph)
+    print(relations)
+    #Initialisation du resultat
+    nodes_in_mst = {start_id}
+    mst_edges = []
+    total_weight = 0
+
+    graph_nodes = set(graph.keys())
+
+    while nodes_in_mst != graph_nodes:
+        #On cherche le plus petit poids de l'arbre
+        min_edge = None
+        for node in nodes_in_mst:
+            #on cherche l'arete de poids minimum
+            for neighbor, weight in graph[node]:
+                if neighbor not in nodes_in_mst:
+                    if min_edge is None or weight < min_edge[2]:
+                        min_edge = (node, neighbor, weight)
+
+        if min_edge is None:
+            print("Le graphe n'est pas connexe")
+        
+        u,v, weight = min_edge
+        mst_edges.append(min_edge)
+        nodes_in_mst.add(v)
+        total_weight += weight
+
+        return mst_edges, nodes_in_mst, total_weight
+    # #tant que  tout les noeuds de l'arbre ne sont pas explorés
+    # while set(nodes_in_mst) != set(graph):
+    #     #On cherche le plus petit poids de l'arbre
+    #     min_edge = None
+    #     for node in nodes_in_mst:
+    #         for neighbor in graph:
+    #             if neighbor not in nodes_in_mst:
+    #                 if min_edge is None :
+    #                     min_edge = (node, neighbor)
+
+       
     pass  # À implémenter
 
 def kruskal():
